d9e5e034b637406cd8154fa4ab9dd7b106dfd8ee
Kirill Mokevnin <mokevnin@gmail.com>
2016-06-04 20:52:09 +0400

    remove lists

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import * as listModule from './list';
-import * as setModule from './set';
-import * as pairModule from './pair';
+export const cons = (a, b) => (message) => {
+  switch (message) {
+    case 'car':
+      return a;
+    case 'cdr':
+      return b;
+    default:
+      throw new Error(`Unknown message '${message}'`);
+  }
+};
 
-export const cons = pairModule.cons;
-export const car = pairModule.car;
-export const cdr = pairModule.cdr;
-export const toString = pairModule.toString;
+export const car = (pair) => pair('car');
+export const cdr = (pair) => pair('cdr');
 
-export const list = listModule;
-export const set = setModule;
+export const isPair = (pair) => typeof pair === 'function';
+
+export const toString = (pair) => {
+  const rec = (p) => {
+    if (pair === null) {
+      return '';
+    }
+    const head = car(p);
+    const tail = cdr(p);
+    if (isPair(tail)) {
+      return `${head}, ${rec(tail)}`;
+    } else if (tail === null) {
+      return head;
+    }
+    return `${head}, ${tail}`;
+  };
+
+  return `(${rec(pair)})`;
+};
