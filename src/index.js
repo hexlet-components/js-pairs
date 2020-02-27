1149ca40f6a559988ca71db1248b584dd9766f70
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-02-27 22:28:12 +0200

    remove flow-typed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-// @flow
-
-type Message = 'car' | 'cdr';
-
-type Pair = (message: Message) => any;
-
 /**
  * Check if something is pair
  * @example
@@ -11,9 +5,9 @@ type Pair = (message: Message) => any;
  * isPair(pair); // true
  * isPair(5); // false
  */
-export const isPair = (pair: ?Pair): boolean => typeof pair === 'function' && pair.pair;
+export const isPair = (pair) => typeof pair === 'function' && pair.pair;
 
-export const checkPair = (pair: ?Pair): void => {
+export const checkPair = (pair) => {
   if (!isPair(pair)) {
     const value = typeof pair === 'object' ? JSON.stringify(pair, null, 2) : String(pair);
     throw new Error(`Argument must be pair, but it was '${value}'`);
@@ -27,8 +21,8 @@ export const checkPair = (pair: ?Pair): void => {
  * @example
  * const pair = cons(cons(1, null), 'world');
  */
-export const cons = (a: any, b: any): Pair => {
-  const pair = (message: Message) => {
+export const cons = (a, b) => {
+  const pair = (message) => {
     switch (message) {
       case 'car':
         return a;
@@ -48,7 +42,7 @@ export const cons = (a: any, b: any): Pair => {
  * const pair = cons(5, 'hello');
  * car(pair); // 5
  */
-export const car = (pair: Pair): any => {
+export const car = (pair) => {
   checkPair(pair);
   return pair('car');
 };
@@ -59,7 +53,7 @@ export const car = (pair: Pair): any => {
  * const pair = cons(5, 'hello');
  * cdr(pair); // hello
  */
-export const cdr = (pair: Pair): any => {
+export const cdr = (pair) => {
   checkPair(pair);
   return pair('cdr');
 };
@@ -69,7 +63,7 @@ export const cdr = (pair: Pair): any => {
  * @example
  * toString(cons('', 10)); // ('', 10)
  */
-export const toString = (pair: Pair): string => {
+export const toString = (pair) => {
   checkPair(pair);
   const rec = (p) => {
     if (!isPair(p)) {
